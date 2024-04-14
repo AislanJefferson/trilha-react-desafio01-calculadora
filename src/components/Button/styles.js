945718fed53a66de14d33b7f1651a5c1296b4b9d
AislanJefferson <aislanjsb@gmail.com>
2024-04-13 21:22:09 -0300

    Inserido codigo fonte desenvolvido

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,15 +1,15 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
-    padding: 20px;
-    border: 1px solid #CDCDCD;
-    background-color: #00AAF0;
-    color: #FFFFFF;
-    font-size: 24px;
-    font-weight: 700;
-    flex: 1;
-    
-    &:hover {
-        opacity: 0.6;
-    }
-`
+  padding: 20px;
+  border: 1px solid #c0c0c0c0;
+  background-color: #00aaff;
+  color: #ffffff;
+  font-size: 24px;
+  font-weight: 700;
+  flex: 1;
+
+  &:hover {
+    opacity: 0.9;
+  }
+`;
--- src/styles.js
+++ src/styles.js
@@ -1,32 +1,30 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: #CACACA;
+  width: 100%;
+  height: 100vh;
+  background-color: #fafa;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const Content = styled.div`
-    background-color: #FFFFFF;
-    width: 50%;
-`
+  background-color: #ffcc;
+  width: 50%;
+`;
 
 export const Row = styled.div`
-    display:flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 export const Column = styled.div`
-    display:flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-`
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
